fb79fe183aec2e06cf5f1410b3ca57d96a52e379
railsmechanic <matthias.kalb@railsmechanic.de>
2019-12-22 22:22:24 +0100

    Refactored moduledoc

--- lib/zachaeus/plug/ensure_authenticated.ex
+++ lib/zachaeus/plug/ensure_authenticated.ex
@@ -1,12 +1,24 @@
 if Code.ensure_loaded?(Plug) do
   defmodule Zachaeus.Plug.EnsureAuthenticated do
     @moduledoc """
-    This plug ensures that a valid token was provided and has been verified on the request.
+    This plug ensures that a valid and unexpired license was provided on the request.
+    If the license could not be found, has expired or is just invalid, it returns a JSON error representation.
+    You can use this plug in your own application e.g. within the authentication pipeline.
 
-      If one is not found, the `auth_error` will be called with `:unauthenticated`
+    For example:
+      defmodule MyAppWeb.Router do
+        use Phoenix.Router
 
-    This, like all other Guardian plugs, requires a Guardian pipeline to be setup.
-    It requires an implementation module, an error handler and a key.
+        pipeline :api do
+          plug :accepts, ["json"]
+          plug Zachaeus.Plug.EnsureAuthenticated
+        end
+
+        scope "/" do
+          pipe_through :api
+          # API related routes...
+        end
+      end
     """
     use Zachaeus.Plug
 
