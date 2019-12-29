75b08f99b5b3d49fba2f1b6abf814e9fa8a140bd
railsmechanic <matthias.kalb@railsmechanic.de>
2019-12-29 17:23:24 +0100

    Refactored key generation mix task

--- lib/mix/tasks/zachaeus.gen.keys.ex
+++ lib/mix/tasks/zachaeus.gen.keys.ex
@@ -1,74 +1,77 @@
 defmodule Mix.Tasks.Zachaeus.Gen.Keys do
+  @shortdoc "Generates the public/secret key pair"
+
   @moduledoc """
-  Generate Coherence controllers.
-  Use this task to generate the Coherence controllers when
-  you what to customize the controllers' behavior.
-  ## Examples
-      # Install all the controllers for the coherence opts
-      # defined in your `config/config.exs` file
-      mix coh.gen.controllers
-  Once the controllers have been generated, you must update your
-  `router.ex` file to properly scope the generated controller(s).
-  For example:
-      # lib/my_app_web
-      def MyAppWeb.Router do
-        # ...
-        scope "/", MyAppWeb do
-          pipe_through :browser
-          coherence_routes()
-        end
-        scope "/", MyAppWeb do
-          pipe_through :protected
-          coherence_routes :protected
-        end
-        # ...
-      end
-  ## Options
-  * `--web-path` override the web path
-  * `--no-confirm` silently overwrite files
+  Generates a Zachaeus key pair.
+
+      mix zachaeus.gen.keys
+
+  Generation of a public/secret key pair in order to sign/verify licenses.
+  The generated key pair can to be stored in your `config.exs` according to the instructions.
+
+
+  This task does not require any arguments to work.
   """
   use Mix.Task
 
-  @shortdoc "Generates the required public/secret key pair"
-
+  @doc false
   @impl Mix.Task
   def run(_args) do
-    with {:ok, _} <- Application.ensure_all_started(:salty),
+    with {:ok, _salty_started} <- Application.ensure_all_started(:salty),
          {:ok, raw_public_key, raw_secret_key} <- Salty.Sign.Ed25519.keypair(),
          {:ok, public_key} <- encode_key(raw_public_key),
          {:ok, secret_key} <- encode_key(raw_secret_key)
     do
       Mix.shell().info("""
-
       Modify your config.exs file and add the following configuration as indicated below:
-
-        config :zachaeus,
-          public_key: "#{public_key}",
-          secret_key: "#{secret_key}"
-
-
-      If you only want to verify licenses, modify your config.exs file and add the following configuration as indicated below:
-
-        config :zachaeus,
-          public_key: "#{public_key}"
-
-      For generating licenses, modify your config.exs file and add the following configuration as indicated below:
-
-        config :zachaeus,
-          secret_key: "#{secret_key}"
-
-
-      >> Please make a backup of your keys, otherwise you will lose the ability to verify already issued licenses. <<
       """)
+
+      Mix.shell().info([
+        :green,
+        """
+          config :zachaeus,
+              public_key: "#{public_key}",
+              secret_key: "#{secret_key}"
+        """
+      ])
+
+      Mix.shell().info("""
+      If you only want to verify licenses, modify your config.exs file and add the following configuration as indicated below:
+      """)
+
+      Mix.shell().info([
+        :green,
+        """
+          config :zachaeus,
+            public_key: "#{public_key}"
+        """
+      ])
+
+      Mix.shell().info("""
+      For generating licenses, modify your config.exs file and add the following configuration as indicated below:
+      """)
+
+      Mix.shell().info([
+        :green,
+        """
+          config :zachaeus,
+            secret_key: "#{secret_key}"
+        """
+      ])
+
+      Mix.shell().info([
+        :red,
+        """
+        HINT: Please make a backup of both keys, otherwise you will lose the ability to verify (already issued) licenses.
+        """
+      ])
     else
       {:error, message} ->
-        Mix.raise(
-          """
-          Unable to generate public/secret key pair due to the following error:
+        Mix.raise("""
+        ERROR: #{message}
 
-          #{message}
-          """
-        )
+        Unable to generate public/secret key pair due to the following error:
+        """)
     end
   end
 
@@ -76,7 +79,7 @@ defmodule Mix.Tasks.Zachaeus.Gen.Keys do
   @spec encode_key(key :: binary()) :: {:ok, String.t()} | {:error, String.t()}
   defp encode_key(key) when is_binary(key) and byte_size(key) > 0,
     do: {:ok, Base.url_encode64(key, padding: false)}
-  defp encode_key(_invalid_key),
-    do: {:error, "Unable to encode key(s) due to invalid data"}
 
+  defp encode_key(_invalid_key),
+    do: {:error, "Unable to encode keys due to invalid data"}
 end
