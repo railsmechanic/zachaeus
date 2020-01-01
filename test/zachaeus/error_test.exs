c4d156dd9cf6d20473a071808d3c5d477f9c7f65
railsmechanic <matthias.kalb@railsmechanic.de>
2020-01-01 19:24:03 +0100

    Refactored error test

--- test/zachaeus/error_test.exs
+++ test/zachaeus/error_test.exs
@@ -2,7 +2,7 @@ defmodule Zachaeus.ErrorTest do
   use ExUnit.Case, async: true
   doctest Zachaeus.Error
 
-  test "for the required fields" do
+  test "for required fields" do
     error = %Zachaeus.Error{}
     assert Map.has_key?(error, :code)
     assert Map.has_key?(error, :message)
